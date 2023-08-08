e298b54d830f5252d28a0263b106163e96332dfa
Manuel Vogel <mavogel@posteo.de>
2023-08-08 11:29:55 +0200

    fix: add ses receive stack tests

---
+++ test/ses-receive-stack.test.ts
@@ -0,0 +1,33 @@
+import {
+  App,
+  Stack,
+  aws_s3 as s3,
+} from 'aws-cdk-lib';
+import { Template } from 'aws-cdk-lib/assertions';
+import { SESReceiveStack, SESReceiveStackProps } from '../src';
+
+test('ses-receive-stack-create', () => {
+  const app = new App();
+  const stack = new Stack(app, 'testStack', {
+    env: {
+      region: 'eu-west-1',
+      account: '1234',
+    },
+  });
+
+  const emailBucket = new s3.Bucket(stack, 'EmailBucket', {
+    bucketName: 'test-email-bucket',
+    blockPublicAccess: s3.BlockPublicAccess.BLOCK_ALL,
+  });
+
+  const testProps: SESReceiveStackProps = {
+    domain: 'example.com',
+    subdomain: 'aws',
+    emailbucket: emailBucket,
+  };
+
+  new SESReceiveStack(stack, 'testSesReceive', testProps);
+
+  const template = Template.fromStack(stack);
+  expect(template.toJSON()).toMatchSnapshot();
+});
