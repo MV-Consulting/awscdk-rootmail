3a2baa61a3113ba00f8e35931ba6f312988922f3
Manuel Vogel <8409778+mavogel@users.noreply.github.com>
2024-11-24 19:34:54 +0100

    feat: add integ test run in gh actions
    Signed-off-by: Manuel Vogel <8409778+mavogel@users.noreply.github.com>

--- .projenrc.ts
+++ .projenrc.ts
@@ -150,12 +150,69 @@ const buildAndPublishAssetsSteps = [
 
 if (buildWorkflow) {
   buildWorkflow.addJobs({
+    e2e_integ_test: {
+      name: 'Run e2e integ tests',
+      runsOn: ['ubuntu-latest'],
+      permissions: {
+        idToken: JobPermission.WRITE,
+        contents: JobPermission.READ,
+      },
+      // self-mutation did not happen and the PR is from the same repo
+      if: '!(needs.build.outputs.self_mutation_happened) && !(github.event.pull_request.head.repo.full_name != github.repository)',
+      steps: [
+        {
+          name: 'Checkout',
+          uses: 'actions/checkout@v4',
+        },
+        {
+          name: 'Configure AWS credentials',
+          uses: 'aws-actions/configure-aws-credentials@v4',
+          with: {
+            'aws-region': 'eu-west-1',
+            'role-to-assume': '${{ secrets.E2E_INTEG_ROLE }}',
+            'role-session-name': 'e2e-integ-test-federatedOIDC',
+          },
+        },
+        {
+          name: 'Setup Node.js',
+          uses: 'actions/setup-node@v4',
+          with: {
+            'node-version': '20.x',
+          },
+        },
+        {
+          name: 'Install dependencies',
+          run: 'yarn install --check-files',
+        },
+        {
+          name: 'Run e2e integ tests',
+          run: 'yarn run integ-test',
+          timeoutMinutes: 10,
+        },
+        {
+          name: 'Install Python dependencies',
+          run: 'pip install -r requirements.txt',
+          workingDirectory: 'integ-tests',
+        },
+        {
+          name: 'Post e2e integ tests cleanup',
+          run: [
+            `for bucket in $(aws s3 ls | grep rootmailinteg |  awk '{ print $3 }'); do echo $bucket; python cleanup/empty-and-delete-s3-bucket.py $bucket; done`,
+            `for lgregion in eu-west-1 eu-west-2; do echo $lgregion; python cleanup/delete-log-groups.py Integ $lgregion; done`
+          ].join('\n'),
+          workingDirectory: 'integ-tests',
+        }
+      ],
+    }
+  });
+}
+
+if (buildWorkflow) {
+  buildWorkflow.addJobs({
     release_s3_dev: {
       name: 'Publish to S3 (dev)',
       runsOn: ['ubuntu-latest'],
-      needs: ['build'],
-      // self-mutation did not happen and the PR is from the same repo
-      if: '!(needs.build.outputs.self_mutation_happened) && !(github.event.pull_request.head.repo.full_name != github.repository)',
+      needs: ['e2e_integ_test'],
       permissions: {
         idToken: JobPermission.WRITE,
         contents: JobPermission.READ,
